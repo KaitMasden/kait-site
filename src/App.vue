1bd99046d5f5e0e6c541f001c4a19e30b309cf9e
miguel paredes <kait.masden@gmail.com>
2019-07-01 18:51:42 -0400

    shits broken already

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <NavBar />
-    </div>
+    <NavBar />
     <router-view />
   </div>
 </template>
 
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
-html, body {
-  font-family: 'Roboto Condensed', sans-serif;
-  background-color: #F4F4F4;
-}
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
+  @import url('.\styles\scss\variables.scss');
 
-#app {
-  font-family: 'Roboto Condensed', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #373737;
-}
-#nav {
-  padding: 30px;
-  background-color: #373737;
-  color: #C0B283;
-  word-spacing: 30px;
-  a {
-    font-weight: bold;
-    text-decoration: none; 
-    text-transform: uppercase;
-    color: #C0B283;
-    &.router-link-exact-active {
-      padding:10px;
-      background-color:#C0B283;
-      color: #373737;
-    }
+  html, body {
+    font-family: 'Roboto Condensed', sans-serif;
+    background-color: #F4F4F4;
+    margin:0px;
   }
-}
+
+  #app {
+    font-family: 'Roboto Condensed', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #373737;
+  }
+  
 </style>
 
 <script>
--- src/components/molecules/NavBar.vue
+++ src/components/molecules/NavBar.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>
+  <nav> 
     <router-link to="/">Home</router-link>   | 
     <router-link to="/about">About</router-link> | 
     <router-link to="/skills">Skills</router-link> | 
     <router-link to="/experience">Experience</router-link> | 
     <router-link to="/portfolio">Portfolio</router-link> | 
     <router-link to="/contact">Contact</router-link>
-  </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
-
+  @import "styles/scss/variables";
+  nav {
+    padding: 30px;
+    background-color: #373737;
+    color: #C0B283;
+    word-spacing: 30px;
+    a {
+      font-weight: bold;
+      text-decoration: none; 
+      text-transform: uppercase;
+      color: #C0B283;
+      &.router-link-exact-active {
+        padding:10px;
+        background-color:#C0B283;
+        color: #373737;
+      }
+    }
+  }
 </style>
 
 <script>
